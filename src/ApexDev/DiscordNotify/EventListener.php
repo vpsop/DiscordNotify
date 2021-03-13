2fcc60ec84490ce974855ba0f76b13dc79a1ec58
Varun <vpsop544@gmail.com>
2021-03-13 23:25:22 +0530

    add listener to PlayerChatEvent

--- src/ApexDev/DiscordNotify/EventListener.php
+++ src/ApexDev/DiscordNotify/EventListener.php
@@ -6,6 +6,8 @@ namespace ApexDev\DiscordNotify;
 
 
 use pocketmine\event\Listener;
+use pocketmine\event\player\cheat\PlayerCheatEvent;
+use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 
@@ -29,5 +31,12 @@ class EventListener implements Listener
         $eventType = "quit";
         $name = $e->getPlayer()->getName();
         $this->plugin->sendDiscordMsg($name, $eventType);
+    }
+
+    public function onPlayerChat(PlayerChatEvent $e)
+    {
+        $name = $e->getPlayer()->getName();
+        $chatMsg = $e->getMessage();
+        $this->plugin->sendDiscordChat($name, $chatMsg);
     }
 }
